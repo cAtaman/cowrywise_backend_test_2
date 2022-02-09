aa6f6cef74eb11fbb967b99e289f63f9094af02e
cAtaman <chimaataman@gmail.com>
2022-02-09 18:56:25 +0100

    add elevated.py on both admin and client packages to handle internal calls

---
+++ src/admin/elevated.py
@@ -0,0 +1,50 @@
+"""This module handles response to system calls from the Client API"""
+import typing as t
+from datetime import datetime, timedelta
+
+from .models import Book, Borrow, BorrowSchema, User, UserSchema
+from .wsgi import db
+
+
+def borrow(book_id: int, duration: int, user_id: int) -> t.Tuple[str, int]:
+    # create a borrow entry
+    new_borrow_dict = {
+        "book_id": book_id,
+        "user_id": user_id,
+        "borrow_date": datetime.now().date(),
+        "duration": duration,
+        "return_date": datetime.now().date() + timedelta(duration)
+    }
+    new_borrow = BorrowSchema().load(new_borrow_dict)
+    db.session.add(new_borrow)
+    db.session.commit()
+
+    # update book
+    book = Book.query.filter_by(book_id=book_id).first()
+    borrow = Borrow.query.filter_by(**new_borrow_dict).first()
+
+    if not (book and borrow):
+        return "Something went wrong", 404
+
+    book.current_borrow_id = borrow.id
+    book.available = False
+    db.session.add(book)
+    db.session.commit()
+    return "Borrow successful", 200
+
+
+def enrol(user_data: dict) -> t.Tuple[str, int]:
+    new_user_dict = {
+        "email": user_data.get("email"),
+        "first_name": user_data.get("first_name"),
+        "last_name": user_data.get("last_name"),
+        "password": user_data.get("password")
+    }
+    email = new_user_dict["email"]
+    user = User.query.filter_by(email=email).first()
+    if user:
+        return f"User with email {email} already exists", 400
+    new_user = UserSchema(new_user_dict).load()
+    db.session.add(new_user)
+    db.session.commit()
+    return "Enrolment successful", 200
---
+++ src/client/elevated.py
@@ -0,0 +1,22 @@
+"""This module handles response to system calls from Admin API"""
+import typing as t
+
+from .models import BookSchema, Book
+from .wsgi import db
+
+
+def add_book(book_data: dict) -> t.Tuple[dict, int]:
+    book = BookSchema().load(book_data)
+    db.session.add(book)
+    db.session.commit()
+    return BookSchema().dump(book), 200
+
+
+def remove_book(book_id: int) -> t.Tuple[str, int]:
+    book = Book.query.filter_by(book_id=book_id)
+    if book:
+        db.session.delete(book)
+        db.session.commit()
+        return "Book removed successfully", 200
+    else:
+        return "Book was not in catalogue", 400
